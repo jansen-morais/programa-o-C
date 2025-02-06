a3ebfaa7c300eb9d66530779300fe082d87c77b9
jansen-morais <jansenmorais86@gmail.com>
2025-02-06 19:31:47 +0000

    adcionei o fgets e estou testando algumas funçoes.

--- Teste de interação e coleta de informaçoes.c
+++ Teste de interação e coleta de informaçoes.c
@@ -2,7 +2,8 @@
 
 int main(){
 
-    char nome[30];
+    char opcao ='s';
+    char nome[50];
     char natural[30];
     int idade;
     char profissao[99];
@@ -13,14 +14,16 @@ int main(){
     printf("Olá!\n Tudo bem?\n ... \n Eespero que sim...\n");
     printf(":)\n.\n.");
     printf("Vamos lá.... \nPrimeiramente voçê porderia se aprensentar?\n");
-    scanf(" %s");//só para interção, antes de iniciar a coleta de informaçoes.
+    // testando ainda, |scanf(" %c");//só para interção, antes de iniciar a coleta de informaçoes.
     printf("Me fale seu nome\n:");
-    scanf("%29s",&nome);
+    fgets(nome,sizeof(nome),stdin);
     printf("Agora sua idade\n:");scanf("%d,\n",&idade);
     printf("Agora sua altura\n:"); scanf("%f",&altura); printf("Onde voçê nasceu?\n:"); scanf(" %29s,\n",&natural);
     printf("Qual sua profissão?\n:"); scanf(" %99s,\n",&profissao);
     printf("E o seu saldo?\n:"); scanf("%39s",&saldo); printf("perfeito...\n...\n");
+    //aqui vai imprimir na tela as informações coletadas.
     printf("Nome: %s\nIdade: %d\nAltura: %.2f\nNatural: %s\nProfissão: %s\nSaldo: %s\n",nome,idade,altura,natural,profissao,saldo);
+    //aqui esta avendo um erro bem estranho onde a resposta está pulando duas linhas, porem quando apago a função, \n, funciona normal.. preciso observar isso futuramente.
     printf("...\nCorreto...?\n"); //só para interação
     printf("Prontinho...\n :)\n, Muito obrigado!\n,.."); 
 
