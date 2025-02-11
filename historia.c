7e3135e4bd7153a3b17080ea41ed7b1eab402ec7
jansen-morais <jansenmorais86@gmail.com>
2025-02-11 13:17:41 +0000

    teste de uma do print_digitado... fase de testes

---
+++ historia.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h> //biblioteca para manipulação de strings
+#include <ctype.h> //biblioteca para funçoes de caracters.(tolower)
+#include <unistd.h> //biblioteca para manipulaçao do tempo(sleep), simula tempo de espera
+#include <stdlib.h> //Inclui a biblioteca stdlib.h, que contém funções gerais, como atoi 
+//(para converter uma string para um inteiro).
+#include <stdarg.h>//paa lidar com a manipulação de argumentos variaveis
+
+void limpar_buffer() { //serve para limpara os espaçoes de linha de uma forma mais massiva 
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    // Verificação adicional: se o buffer não estiver vazio após a limpeza,
+    // descarta o restante dos caracteres
+    if (ungetc(c, stdin) != EOF) { // Devolve o último caractere lido para o buffer
+    while ((c = getchar()) != '\n' && c != EOF); //limpa o espaço em branco deixado
+    }
+}
+int print_digitado(const char *format, ...){//função personalizada para imprimir com efeito de digitação
+//(automatização), pode substituir a função "printf" para dar o efito de digitação no testo.
+    va_list args;
+    va_start(args, format);
+    char buffer[1024];//buffer de amazenamento da string formatada
+    int len = vsnprintf(buffer, sizeof(buffer), format, args);
+
+    for (int i=0; i ,len; i++){//imprime cada caractere com artraso
+        printf("%c", buffer[i]);
+        fflush(stdout);
+        usleep(50000); //poder ajustado para o tempo de impreção dos caracteres 
+
+    }
+    va_end(args);
+    return len;
+}    
+
+    int main(){
+        //agora para que o testo saia com efeito de "digitado"
+        //usaremos o print_digitado ao enves d printf
+
+        print_digitado("Ola, tudo bem!\n...\n");
+        getchar();
+        sleep(1.5);
+
+    char nome[50];
+        print_digitado("Como vc se chama?\n");
+        fgets(nome, sizeof(nome),stdin);
+        nome[strcspn(nome, "\n")] = 0;
+        
+        sleep(1.5);
+        print_digitado("...\n");
+        sleep(1.5);
+        print_digitado("Entendi, %s, nome legal...:)\n",nome);
+
+
+
+    
+
+    }
