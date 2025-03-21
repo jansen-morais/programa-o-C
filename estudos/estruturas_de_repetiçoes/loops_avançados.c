877bfacf8ae4c824a33597c08507dcdbbc5200c6
jansen-morais <jansenmorais86@gmail.com>
2025-03-21 02:02:57 +0000

    comentarios

--- estudos/estruturas_de_repetiçoes/loops_avançados.c
+++ estudos/estruturas_de_repetiçoes/loops_avançados.c
@@ -14,7 +14,9 @@ for (int i = 0, j = 10; i < 5 && j > 5; i++, j--)
 }
 
 //um exemplo de loops com atualizações complexas!
-for (int i = 0; i < 100; i += (i % 2 == 0) ? 1 : 2) 
+for (int i = 0; 
+    i < 100; 
+    i += (i % 2 == 0) ? 1 : 2) // oprador ternario
 {
     printf("%d ", i);
 }
