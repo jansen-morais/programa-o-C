0030698cd115a846d2ff321839d2b0de9401ca9a
jansen-morais <jansenmorais86@gmail.com>
2025-03-21 01:59:10 +0000

    loos avançados

---
+++ estudos/estruturas_de_repetiçoes/loops_avançados.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+
+//um exemplo de loop com múltiplas variáveis!
+for (int i = 0, j = 10; i < j; i++, j--) 
+{
+    printf("i = %d, j = %d\n", i, j);
+}
+
+// um exemplo de loops com condições múltiplas!    
+for (int i = 0, j = 10; i < 5 && j > 5; i++, j--) 
+{
+    printf("i = %d, j = %d\n", i, j);
+}
+
+//um exemplo de loops com atualizações complexas!
+for (int i = 0; i < 100; i += (i % 2 == 0) ? 1 : 2) 
+{
+    printf("%d ", i);
+}
+    return 0;
+}
